b956beb19d1db46967a1269c62a172c03deb3266
ukokuja <ukokuja@gmail.com>
2020-09-28 22:59:03 +0300

    adds udp client

---
+++ udp_client.c
@@ -0,0 +1,41 @@
+// udp client driver program
+#include <stdio.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+#define PORT 1234
+#define MAXLINE 1000
+
+// Driver code
+int main()
+{
+    char *message = "Hello Server"; // TODO: This needs to come from the system monitor
+    int sockfd, n;
+    struct sockaddr_in servaddr;
+
+    // clear servaddr
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //TODO: This needs to come from the system monitor
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_family = AF_INET;
+
+    // create UDP Socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // connect to server
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("\n Error : Connect Failed \n");
+        exit(0);
+    }
+
+    sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr));
+
+    // close the descriptor
+    close(sockfd);
+}
